246d23263ef5dc0637b3e2f6b632b2c108fff6ef
Celebro01 <dangerveneta@gmail.com>
2020-09-23 14:52:37 -0300

    logoff

---
+++ logoff.php
@@ -0,0 +1,22 @@
+<?php 
+
+    session_start();
+
+    //remover indices do array de sessão
+    //unset()
+    foreach($_SESSION as $indice => $item){
+        unset($_SESSION[$indice]);
+    }
+    header('Location: index.php');
+
+
+    //destruir a variavel de sessão
+
+/*     session_destroy();
+
+
+    header('Location: index.php'); 
+
+ */
+
+?>
